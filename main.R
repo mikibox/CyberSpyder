4c0efba7f866cacb52448929f24e2f90923b4b68
mikibox <miquelfairmanfaiman@gmail.com>
2018-05-29 12:39:08 +0200

    adding line plot for all three datasets

--- main.R
+++ main.R
@@ -67,11 +67,11 @@ process_dates <- function(datafinal, dataframe, datecolumn, dateformat, datatype
   datafinal <- bind_rows(datafinal,dfresult)
 }
 
-head(c(breaches, attacks2018))
+head(c(breaches, attacks2017))
 
 spyder <- data.frame(row.names = c('TYPE','YEAR', 'MONTH', 'DAY', 'julianday'))
 spyder <- process_dates(spyder, breaches, "BreachDate", "%Y-%m-%d", "breaches")
-spyder <- process_dates(spyder, attacks2018, "Date", "%d/%m/%Y", "attacks")
+# spyder <- process_dates(spyder, attacks2018, "Date", "%d/%m/%Y", "attacks")
 spyder <- process_dates(spyder, attacks2017, "Date", "%d/%m/%Y", "attacks")
 spyder <- process_dates(spyder, cves, "published.date", "%d/%m/%Y", "cves")
 
@@ -87,6 +87,7 @@ spyderbymonth <-
   summarise(COUNT = sum(!is.na(TYPE)),
             PER = COUNT / spyderTotals$input_total[spyderTotals$input_type == TYPE][1])
 
+spyderbymonth$PER <- lapply(spyderbymonth$PER, function(x) x*100)
 spyderbymonthyear <- spyder %>% group_by(TYPE,MONTH, YEAR) %>% summarise(COUNT = sum(!is.na(MONTH)))%>% mutate(PERCENTAGE = COUNT/sum(COUNT))
 
 spyderbymonthspred <- spread(spyderbymonth[c("TYPE","MONTH","PER")], TYPE, PER)
@@ -111,33 +112,13 @@ spyderbymonthyear %>%
        y = "Daily precipitation (inches)",
        x = "Date") + theme_bw(base_size = 15)
 
-# adjust the x axis breaks
-# scale_x_date(date_breaks = "5 years", date_labels = "%m-%Y")
 
-library(plotly)
-
-month <- c('January', 'February', 'March', 'April', 'May', 'June', 'July',
-           'August', 'September', 'October', 'November', 'December')
-high_2000 <- c(32.5, 37.6, 49.9, 53.0, 69.1, 75.4, 76.5, 76.6, 70.7, 60.6, 45.1, 29.3)
-low_2000 <- c(13.8, 22.3, 32.5, 37.2, 49.9, 56.1, 57.7, 58.3, 51.2, 42.8, 31.6, 15.9)
-high_2007 <- c(36.5, 26.6, 43.6, 52.3, 71.5, 81.4, 80.5, 82.2, 76.0, 67.3, 46.1, 35.0)
-low_2007 <- c(23.6, 14.0, 27.0, 36.8, 47.6, 57.7, 58.9, 61.2, 53.3, 48.5, 31.0, 23.6)
-high_2014 <- c(28.8, 28.5, 37.0, 56.8, 69.7, 79.7, 78.5, 77.8, 74.1, 62.6, 45.3, 39.9)
-low_2014 <- c(12.7, 14.3, 18.6, 35.5, 49.9, 58.0, 60.0, 58.6, 51.7, 45.2, 32.2, 29.1)
-
-data <- data.frame(month, high_2000, low_2000, high_2007, low_2007, high_2014, low_2014)
-
-#The default order will be alphabetized unless specified as below:
-data$month <- factor(data$month, levels = data[["month"]])
-plot_ly(data, x = ~month, y = ~high_2014, name = 'High 2014', type = 'scatter', mode = 'lines',
-        line = list(color = 'rgb(205, 12, 24)', width = 4)) %>%
-  add_trace(y = ~low_2014, name = 'Low 2014', line = list(color = 'rgb(22, 96, 167)', width = 4)) %>%
-  add_trace(y = ~high_2007, name = 'High 2007', line = list(color = 'rgb(205, 12, 24)', width = 4, dash = 'dash')) %>%
-  add_trace(y = ~low_2007, name = 'Low 2007', line = list(color = 'rgb(22, 96, 167)', width = 4, dash = 'dash')) %>%
-  add_trace(y = ~high_2000, name = 'High 2000', line = list(color = 'rgb(205, 12, 24)', width = 4, dash = 'dot')) %>%
-  add_trace(y = ~low_2000, name = 'Low 2000', line = list(color = 'rgb(22, 96, 167)', width = 4, dash = 'dot')) %>%
-  layout(title = "Average High and Low Temperatures in New York",
+spyderbymonthspred$month <- month
+spyderbymonthspred$month <- factor(spyderbymonthspred$month, levels = spyderbymonthspred[["month"]])
+plot_ly(spyderbymonthspred, x = ~month, 
+        y = ~attacks, name = 'attacks', type = 'scatter', mode = 'lines', line = list(color = 'rgb(22,255,13)', width = 4)) %>%
+  add_trace(y = ~cves, name = 'cves', line = list(color = 'rgb(0,148,255)', width = 4)) %>%
+  add_trace(y = ~breaches, name = 'breaches', line = list(color = 'rgb(255,56,0)', width = 4, dash = 'line')) %>%
+  layout(title = "Percentage of incidents by month",
          xaxis = list(title = "Months"),
-         yaxis = list (title = "Temperature (degrees F)"))
-
-
+         yaxis = list (title = "Percentage (%)"))
